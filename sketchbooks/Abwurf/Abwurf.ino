4bb4c67aa29c9b5ae3e0c22ed36fe9a400905e20
daniel-michalovics <daniel.m.93@gmx.de>
2014-07-14 20:24:54 +0200

    aufgeräumt

--- sketchbooks/Abwurf/Abwurf.ino
+++
@@ -1,9 +0,0 @@
-
-#if DROP_ENABLE    
-      else if(cmd.substring(1).equals("drop") || cmd.substring(1).equals("DROP")){ 
-  
-        digitalWrite(DROP_PIN,HIGH);  
-          
-      } 
-#endif
-
